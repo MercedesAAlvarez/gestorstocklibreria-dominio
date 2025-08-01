1904d23248c781f5dc15a628fd075fbe79d0cc25
MercedesAAlvarez <106325676+MercedesAAlvarez@users.noreply.github.com>
2025-08-01 18:24:18 -0300

    changes: changes and deletions of files for project improvements

--- apps/backend/src/adapters/product/getAllProductsAdapter.ts
+++
@@ -1,9 +0,0 @@
-
-
-import { GetAllProducts } from '@domain/use-cases/GetAllProducts';
-import { MockProductRepository } from '@domain/repositories/mocks/MockProductRepository';
-
-export function getAllProductsAdapter() {
-  const repository = new MockProductRepository();
-  return new GetAllProducts(repository).execute();
-}
